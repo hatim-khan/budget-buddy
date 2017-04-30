1b57de6d2a6a4ca58f5f9023bd1263395968ccb1
Susan Le <SusanLe@Susans-MacBook-Pro.local>
2017-04-30 13:53:34 -0700

    piechart correct data, colors dont work

--- Android/BudgetBuddy/app/src/main/java/area51/budgetbuddy/TrendsFragment.java
+++ Android/BudgetBuddy/app/src/main/java/area51/budgetbuddy/TrendsFragment.java
@@ -41,7 +41,6 @@ public class TrendsFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mPage = getArguments().getInt(ARG_PAGE);
-
     }
 
     @Override
@@ -58,74 +57,54 @@ public class TrendsFragment extends Fragment {
         chart.invalidate();
 
         // PERSONAL PIE CHART
+
+        User currentUser = AppVariables.currentUser;
+
+        int i = 1;
         PieChart PersonalPieChart = (PieChart) view.findViewById(R.id.ppchart);
         ArrayList<Entry> entries = new ArrayList<>();
-        entries.add(new Entry(4f, 0));
-        entries.add(new Entry(8f, 1));
-        entries.add(new Entry(6f, 2));
-        entries.add(new Entry(12f, 3));
-        entries.add(new Entry(18f, 4));
-        entries.add(new Entry(9f, 5));
+        ArrayList<String> plabels = new ArrayList<String>();
+
+        for (Budget budget : currentUser.getPersonalBudgets().values()) {
+            String budgetName = budget.name;
+            plabels.add(budgetName);
+            double personalDouble = budget.getAmountSpentInBudget();
+            float personalAmount = (float) personalDouble;
+            entries.add(new Entry(personalAmount, i));
+            i += 1;
+        }
 
         PieDataSet pdataset = new PieDataSet(entries, "# of Calls");
-
-        ArrayList<String> plabels = new ArrayList<String>();
-        plabels.add("Groceries");
-        plabels.add("Entertainment");
-        plabels.add("Clothes");
-        plabels.add("Take Out");
-        plabels.add("Bills");
-        plabels.add("Other");
 
         PieData pdata = new PieData(plabels, pdataset);
         PersonalPieChart.setDescription("");
         pdataset.setColors(ColorTemplate.COLORFUL_COLORS); //
         PersonalPieChart.setData(pdata);
-
         PersonalPieChart.animateY(2000);
 
         //GROUP PIE CHART
-
         PieChart GroupPieChart = (PieChart) view.findViewById(R.id.gpchart);
         ArrayList<Entry> entriess = new ArrayList<>();
-        entriess.add(new Entry(4f, 0));
-        entriess.add(new Entry(8f, 1));
-        entriess.add(new Entry(6f, 2));
-        entriess.add(new Entry(12f, 3));
-        entriess.add(new Entry(18f, 4));
-        entriess.add(new Entry(9f, 5));
+        ArrayList<String> glabels = new ArrayList<String>();
+        int j = 1;
+
+        for (Budget budget : currentUser.userGroupBudgets().values()) {
+            String gBudgetName = budget.name;
+            glabels.add(gBudgetName);
+            double groupDouble = budget.getAmountSpentInBudget();
+            float groupAmount = (float) groupDouble;
+            entriess.add(new Entry(groupAmount, j));
+            j += 1;
+        }
 
         PieDataSet gdataset = new PieDataSet(entriess, "# of Calls");
-
-        ArrayList<String> glabels = new ArrayList<String>();
-        glabels.add("Groceries");
-        glabels.add("Entertainment");
-        glabels.add("Clothes");
-        glabels.add("Take Out");
-        glabels.add("Bills");
-        glabels.add("Other");
 
         PieData gdata = new PieData(glabels, gdataset);
         GroupPieChart.setDescription("");
         gdataset.setColors(ColorTemplate.COLORFUL_COLORS); //
         GroupPieChart.setData(gdata);
-
         GroupPieChart.animateY(2000);
 
-        /*
-        // TODO: Customize the UI for the Payments Screen here
-        // TODO Susan : replace - this is just here for an example on how to get budget data
-        TextView textView = (TextView) view.findViewById(R.id.textview);
-        User currentUser = AppVariables.currentUser;
-        String budgetsString = new String();
-
-        for (Budget budget : currentUser.userGroupBudgets().values()) {
-            String budgetName = budget.name;
-            budgetsString += budgetName + " " +
-                    budget.getAmountSpentInBudget() + " / "
-                    + budget.getBudgetLimit() + "\n";
-        }
-        textView.setText("Budgets: " + budgetsString);*/
         return view;
     }
 
@@ -152,17 +131,17 @@ public class TrendsFragment extends Fragment {
         personalBar.add(v1e5);
         BarEntry v1e6 = new BarEntry(100.000f, 5); // Jun
         personalBar.add(v1e6);
-        BarEntry v1e7 = new BarEntry(110.000f, 6); // Jan
+        BarEntry v1e7 = new BarEntry(110.000f, 6); // Jul
         personalBar.add(v1e7);
-        BarEntry v1e8 = new BarEntry(40.000f, 7); // Feb
+        BarEntry v1e8 = new BarEntry(40.000f, 7); // Aug
         personalBar.add(v1e8);
-        BarEntry v1e9 = new BarEntry(60.000f, 8); // Mar
+        BarEntry v1e9 = new BarEntry(60.000f, 8); // Sep
         personalBar.add(v1e9);
-        BarEntry v1e10 = new BarEntry(30.000f, 9); // Apr
+        BarEntry v1e10 = new BarEntry(30.000f, 9); // Oct
         personalBar.add(v1e10);
-        BarEntry v1e11 = new BarEntry(90.000f, 10); // May
+        BarEntry v1e11 = new BarEntry(90.000f, 10); // Nov
         personalBar.add(v1e11);
-        BarEntry v1e12 = new BarEntry(100.000f, 11); // Jun
+        BarEntry v1e12 = new BarEntry(100.000f, 11); // Dec
         personalBar.add(v1e12);
 
         ArrayList<BarEntry> groupBar = new ArrayList<>();
@@ -178,17 +157,17 @@ public class TrendsFragment extends Fragment {
         groupBar.add(v2e5);
         BarEntry v2e6 = new BarEntry(80.000f, 5); // Jun
         groupBar.add(v2e6);
-        BarEntry v2e7 = new BarEntry(150.000f, 6); // Jan
+        BarEntry v2e7 = new BarEntry(150.000f, 6); // Jul
         groupBar.add(v2e7);
-        BarEntry v2e8 = new BarEntry(90.000f, 7); // Feb
+        BarEntry v2e8 = new BarEntry(90.000f, 7); // Aug
         groupBar.add(v2e8);
-        BarEntry v2e9 = new BarEntry(120.000f, 8); // Mar
+        BarEntry v2e9 = new BarEntry(120.000f, 8); // Sep
         groupBar.add(v2e9);
-        BarEntry v2e10 = new BarEntry(60.000f, 9); // Apr
+        BarEntry v2e10 = new BarEntry(60.000f, 9); // Oct
         groupBar.add(v2e10);
-        BarEntry v2e11 = new BarEntry(20.000f, 10); // May
+        BarEntry v2e11 = new BarEntry(20.000f, 10); // Nov
         groupBar.add(v2e11);
-        BarEntry v2e12 = new BarEntry(80.000f, 11); // Jun
+        BarEntry v2e12 = new BarEntry(80.000f, 11); // Dec
         groupBar.add(v2e12);
 
         BarDataSet barDataSet1 = new BarDataSet(personalBar, "Personal Budget");
